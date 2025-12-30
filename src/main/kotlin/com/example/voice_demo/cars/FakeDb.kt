b7dff2443e249700a41a6ed72cd92e224e7b14a4
Jake Overall <joverall22@gmail.com>
2025-12-30 02:25:07 -0700

    serve at root

--- src/main/kotlin/com/example/voice_demo/cars/FakeDb.kt
+++
@@ -1,8 +0,0 @@
-package com.example.voice_demo.cars
-
-import java.util.UUID
-import java.util.concurrent.ConcurrentHashMap
-
-object FakeDb {
-    val cars: MutableMap<UUID, Car> = ConcurrentHashMap()
-}
--- src/main/kotlin/com/example/voice_demo/config/StaticResourceConfig.kt
+++ src/main/kotlin/com/example/voice_demo/config/StaticResourceConfig.kt
@@ -2,6 +2,7 @@ package com.example.voice_demo.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
@@ -9,5 +10,10 @@ class StaticResourceConfig : WebMvcConfigurer {
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
         registry.addResourceHandler("/**")
             .addResourceLocations("file:src/www/") // point to filesystem folder
+    }
+
+    override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry.addViewController("/")
+            .setViewName("forward:/index.html")
     }
 }
